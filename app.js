71b8b644d8baaa625a9032ceef6368bae1ef9948
MadimetjaMadix <madimetjasethosa@gmail.com>
2020-06-04 16:48:00 +0200

    added a hello world web app

---
+++ app.js
@@ -0,0 +1,12 @@
+var express = require('express')
+var app = express()
+
+// loading our router
+const appRouter = require('./appRoutes.js')
+
+// mounting our router
+app.use('/', appRouter)
+
+const port = process.env.PORT || 3000
+app.listen(port)
+console.log('Express server running on port', port)
---
+++ appRoutes.js
@@ -0,0 +1,8 @@
+var express = require('express')
+const router = express.Router()
+
+router.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+module.exports = router
