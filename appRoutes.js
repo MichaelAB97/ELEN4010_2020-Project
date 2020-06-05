b48e38591a87eeeb68b41adf81805e1a16c36982
MadimetjaMadix <madimetjasethosa@gmail.com>
2020-06-05 11:46:28 +0200

    added about.html file

--- appRoutes.js
+++ appRoutes.js
@@ -1,8 +1,13 @@
+const path = require('path')
 var express = require('express')
 const router = express.Router()
 
 router.get('/', function (req, res) {
   res.send('Hello World!')
+})
+
+router.get('/about', function (req, res) {
+  res.sendFile(path.join(__dirname, 'views', 'about.html'))
 })
 
 module.exports = router
