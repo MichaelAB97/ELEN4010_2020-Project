1ca22d48fad048315165da0f8e1dcba98da9277f
Madimetja Sethosa <41375898+MadimetjaMadix@users.noreply.github.com>
2020-06-09 23:22:49 +0200

    Update app.js

--- app.js
+++ app.js
@@ -6,8 +6,6 @@ const express = require('express')
 const app = express()
 const cookieParser = require('cookie-parser');
 
-app.use(express.urlencoded({ extended: false}));
-app.use(express.json());
 app.use(cookieParser());
 
 // loading body parser
