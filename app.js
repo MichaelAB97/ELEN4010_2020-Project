6c23c6544ff4b1c10469abc391acaa8a0d94dafe
Sicelo Chilaule <chilaulesc@gmail.com>
2020-06-10 10:27:36 +0200

    Configured path to add elements dynamically

--- app.js
+++ app.js
@@ -4,12 +4,12 @@ if (process.env.NODE_ENV = 'development') { require('dotenv').config() }
 const database = require('./modules/database/db-connections') // testing db-connection
 const express = require('express')
 const app = express()
-const cookieParser = require('cookie-parser');
+const cookieParser = require('cookie-parser')
 
-app.use(cookieParser());
+app.use(cookieParser())
 
 // loading body parser
-let bodyParser = require('body-parser')
+const bodyParser = require('body-parser')
 
 // tell express to use body parser for JSON and URL encoded form bodies
 app.use(bodyParser.json())
@@ -20,6 +20,7 @@ const appRouter = require('./appRoutes.js')
 
 // mounting our router
 app.use('/', appRouter)
+app.use('/cdn', express.static('public'))
 
 app.use('/auth', require('./auth.js'))
 
